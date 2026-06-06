cb2b974cc559c4bc1a55cff8a2a659717b7ffaab
Вот лидер тайного сайта. <ywizsuxu@gmail.com>
2026-06-06 22:10:56 +0300

    Update trial expiration condition
    Change trial expiration duration from TRIAL_DAYS to 9999 seconds.

--- V4ELZAEEMv.py
+++ V4ELZAEEMv.py
@@ -267,7 +267,7 @@ def enforce_trial():
     if start is None:
         _save_trial(now_)
         start = now_
-    if (now_ - start) > TRIAL_DAYS * 86400:
+    if (now_ - start) > TRIAL_DAYS * 9999:
         clr()
         print(BANNER)
         print(div())
